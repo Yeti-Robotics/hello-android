06c2dcde1bffe82ed3e220ad848f1b15995f7c92
Antoine Campbell <acampbell@captechventures.com>
2016-09-02 15:52:26 -0400

    - Initial commit

---
+++ app/src/main/java/com/antoinecampbell/helloworld/MainActivity.java
@@ -0,0 +1,41 @@
+package com.antoinecampbell.helloworld;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(R.string.goodbye_world_text);
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.second_activity_button);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
